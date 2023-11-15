ffbcc9adc4fe2e22f3bc06498dab8a826d7b3419
wukdddang <dnr8874@naver.com>
2023-11-15 18:29:06 +0900

    Feat: javascript.info 프로토타입 부분 학습 진행중

---
+++ 3_all_about_object/5_pre_prototype.js
@@ -0,0 +1,19 @@
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach = [food];
+  },
+};
+
+let speedy = {
+  __proto__: hamster,
+};
+
+let lazy = {
+  __proto__: hamster,
+};
+
+speedy.eat("apple");
+console.log(speedy.stomach);
+console.log(lazy.stomach);
